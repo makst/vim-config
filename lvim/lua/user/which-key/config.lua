dd59d2d07861e28ea2d25597480c97740545c76a
Max Tkachenko <makst@users.noreply.github.com>
2023-12-27 01:13:13 +0100

    add find git file mapping

--- lvim/lua/user/which-key/config.lua
+++ lvim/lua/user/which-key/config.lua
@@ -2,6 +2,9 @@ local which_key = {
   mappings = {
     ["e"] = { "<cmd>NvimTreeToggle<CR>", "Explorer" },
     ["f"] = { "<cmd>NvimTreeFindFileToggle<CR>", "Find File Toggle" },
+    s = {
+      g = { "<cmd>Telescope git_files<cr>", "Find Git File" },
+    }
   }
 }
 
